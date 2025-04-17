c89d01c71920bcb89cbd4881e6a0cefaf6664eab
snakee0101 <snakee0101@gmail.com>
2025-04-17 18:58:43 +0300

    transaction category could be created

--- app/Http/Controllers/TransactionCategoryController.php
+++ app/Http/Controllers/TransactionCategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\TransactionCategory;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 use Inertia\Inertia;
 
 class TransactionCategoryController extends Controller
@@ -11,7 +12,9 @@ class TransactionCategoryController extends Controller
     public function index()
     {
         return Inertia::render('transaction_categories', [
-            'transactionCategoryPaginator' => TransactionCategory::where('user_id', auth()->id())->paginate()
+            'transactionCategoryPaginator' => TransactionCategory::where('user_id', auth()->id())
+                                                                ->latest()
+                                                                ->paginate()
         ]);
     }
 
@@ -22,7 +25,19 @@ class TransactionCategoryController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => [
+                'min: 3',
+                Rule::unique('transaction_categories')->where('user_id', auth()->id())
+            ]
+        ]);
+
+        TransactionCategory::create([
+            'user_id' => auth()->id(),
+            'name' => $request->name
+        ]);
+
+        return to_route('transaction_category.index');
     }
 
     public function show(TransactionCategory $transactionCategory)
--- resources/js/pages/transaction_categories.tsx
+++ resources/js/pages/transaction_categories.tsx
@@ -3,10 +3,14 @@ import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
 
 import { router } from '@inertiajs/react';
-import dayjs from 'dayjs';
 import { Pagination, Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from 'flowbite-react';
-import { useState } from 'react';
 import {getPageUrl} from '../lib/helpers';
+
+import { Toast, ToastToggle } from "flowbite-react";
+import { HiCheck } from "react-icons/hi";
+import { Button, Label, Modal, ModalBody, ModalHeader, TextInput, createTheme } from "flowbite-react";
+import { useState } from "react";
+import { useForm } from '@inertiajs/react';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -15,16 +19,42 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-export default function TransactionCategories({ transactionCategoryPaginator }) {
-    console.log(transactionCategoryPaginator);
+const toastThemeWithAbsolutePositioning = createTheme({
+    toast: {
+        root: {
+            base: "absolute top-2 right-2 flex w-full max-w-xs items-center rounded-lg bg-white p-4 text-gray-500 shadow dark:bg-gray-800 dark:text-gray-400",
+            closed: "opacity-0 ease-out"
+        },
+        toggle: {
+            base: "-m-1.5 ml-auto inline-flex h-8 w-8 rounded-lg bg-white p-1.5 text-gray-400 hover:bg-gray-100 hover:text-gray-900 focus:ring-2 focus:ring-gray-300 dark:bg-gray-800 dark:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-white",
+            icon: "h-5 w-5 shrink-0"
+        }
+    },
+});
 
+export default function TransactionCategories({ transactionCategoryPaginator }) {
     const onPageChange = (page: number) => router.visit(
         getPageUrl(transactionCategoryPaginator, page)
     );
 
+    const [isNotificationShown, setIsNotificationShown] = useState(false);
+    const [notificationMessage, setNotificationMessage] = useState('');
+
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
+            {isNotificationShown && <Toast theme={toastThemeWithAbsolutePositioning.toast}>
+                <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
+                    <HiCheck className="h-5 w-5" />
+                </div>
+                <div className="ml-3 text-sm font-normal">{notificationMessage}</div>
+                <ToastToggle />
+            </Toast>}
+
             <Head title="Transaction Categories" />
+
+            <div className='flex mb-4 mx-2'>
+                <CreateCategoryModal setIsNotificationShown={setIsNotificationShown} setNotificationMessage={setNotificationMessage}/>
+            </div>
 
             <Table>
                 <TableHead>
@@ -52,5 +82,64 @@ export default function TransactionCategories({ transactionCategoryPaginator }) 
                 />
             </div>}
         </AppLayout>
+    );
+}
+
+export function CreateCategoryModal({ setIsNotificationShown, setNotificationMessage }) {
+    const [openModal, setOpenModal] = useState(false);
+
+    const { data, setData, post, processing, errors } = useForm({
+        name: '',
+    });
+
+    const onCloseModal = () => {
+        setOpenModal(false);
+        setData('name', '');
+    };
+
+    const handleCreate = (event) => {
+        event.preventDefault();
+
+        post(route('transaction_category.store'), {
+            onSuccess: () => {
+                onCloseModal()
+
+                setNotificationMessage('Transaction category created')
+                setIsNotificationShown(true)
+
+                setTimeout(() => setIsNotificationShown(false), 2000)
+            }
+        });
+    };
+
+    return (
+        <>
+            <Button onClick={() => setOpenModal(true)} size="xs">Create Category</Button>
+
+            <Modal show={openModal} size="md" onClose={onCloseModal} popup>
+                <ModalHeader>
+                    Create New Category
+                </ModalHeader>
+                <ModalBody>
+                    <form className="space-y-6" onSubmit={handleCreate}>
+                        <div>
+                            <Label htmlFor="category-name" value="Category Name" className="mb-2 block" />
+                            <TextInput
+                                id="category-name"
+                                placeholder="Enter category name"
+                                value={data.name}
+                                onChange={(e) => setData('name', e.target.value)}
+                                required
+                            />
+                            {errors && <p className='text-red-600'>{errors.name}</p>}
+                        </div>
+
+                        <div className="w-full flex justify-end">
+                            <Button type="submit">Create</Button>
+                        </div>
+                    </form>
+                </ModalBody>
+            </Modal>
+        </>
     );
 }
