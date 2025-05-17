60ad58cb17ea6ff4468e27fa3d20fec225e6f2da
snakee0101 <snakee0101@gmail.com>
2025-05-17 19:20:11 +0300

    validate transaction amount against savings plan balance - include pending transactions

--- app/Rules/WithinSavingsBalance.php
+++ app/Rules/WithinSavingsBalance.php
@@ -2,7 +2,9 @@
 
 namespace App\Rules;
 
+use App\Enums\TransactionStatus;
 use App\Models\SavingsPlan;
+use App\Models\Transaction;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 
@@ -15,14 +17,31 @@ class WithinSavingsBalance implements ValidationRule
 
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $finalSavingsPlanBalance = $this->amount + $this->savingsPlan->balance;
+        $futureSavingsPlanBalance = $this->savingsPlan->balance + $this->pendingTransactionsTotal();
+        $finalSavingsPlanBalance = $this->amount + $futureSavingsPlanBalance;
 
         if($finalSavingsPlanBalance > $this->savingsPlan->target_balance) {
-            $fail("This transaction exceeds the savings plan balance. Maximum allowed amount: $" . number_format($this->savingsPlan->target_balance - $this->savingsPlan->balance, 2));
+            $fail("This transaction exceeds the savings plan balance (pending transactions included). Maximum allowed amount: $" . number_format($this->savingsPlan->target_balance - $futureSavingsPlanBalance, 2));
         }
 
         if($finalSavingsPlanBalance < 0) {
-            $fail("Can't withdraw more than savings plan balance. Maximum allowed amount: $" . number_format($this->savingsPlan->balance, 2));
+            $fail("Can't withdraw more than savings plan balance (pending transactions included). Maximum allowed amount: $" . number_format($futureSavingsPlanBalance, 2));
         }
+    }
+
+    private function pendingTransactionsTotal()
+    {
+        return Transaction::selectRaw("COALESCE(SUM(CASE WHEN destination_type = ? THEN amount ELSE -amount END), 0) AS total", [SavingsPlan::class])
+                            ->where(function($q) {
+                                $q->where([
+                                    ['source_type', SavingsPlan::class],
+                                    ['source_id', $this->savingsPlan->id]
+                                ])->orWhere([
+                                    ['destination_type', SavingsPlan::class],
+                                    ['destination_id', $this->savingsPlan->id]
+                                ]);
+                            })
+                            ->where('status', TransactionStatus::Pending)
+                            ->value('total');
     }
 }
