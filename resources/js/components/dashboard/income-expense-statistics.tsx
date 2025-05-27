1628b47c139b315dbec655122bf2382293f54e06
snakee0101 <snakee0101@gmail.com>
2025-05-27 21:11:20 +0300

    fixed displaying statistics in dashboard page

--- resources/js/components/dashboard/income-expense-statistics.tsx
+++ resources/js/components/dashboard/income-expense-statistics.tsx
@@ -20,23 +20,23 @@ export default function IncomeExpenseStatistics({statistics}) {
     const stats = [
         {
             title: 'Income',
-            value: `$ ${formatMoney(statistics[1].income)}`,
-            change: `$ ${formatMoney(statistics[1].income_diff)}`,
-            increase: statistics[1].income_diff > 0,
+            value: `$ ${formatMoney(statistics[statistics.length - 1].income)}`, //last item of an array holds statistics for current month
+            change: `$ ${formatMoney(statistics[statistics.length - 1].income_diff)}`,
+            increase: statistics[statistics.length - 1].income_diff > 0,
             icon: <RiCoinsFill size={36} />
         },
         {
             title: 'Expense',
-            value: `$ ${formatMoney(statistics[1].expense)}`,
-            change: `$ ${formatMoney(statistics[1].expense_diff)}`,
-            increase: statistics[1].expense_diff > 0,
+            value: `$ ${formatMoney(statistics[statistics.length - 1].expense)}`,
+            change: `$ ${formatMoney(statistics[statistics.length - 1].expense_diff)}`,
+            increase: statistics[statistics.length - 1].expense_diff > 0,
             icon: <GiPayMoney size={36} />
         },
         {
             title: 'Savings',
-            value: `$ ${formatMoney(statistics[1].savings)}`,
-            change: `$ ${formatMoney(statistics[1].savings_diff)}`,
-            increase: statistics[1].savings_diff > 0,
+            value: `$ ${formatMoney(statistics[statistics.length - 1].savings)}`,
+            change: `$ ${formatMoney(statistics[statistics.length - 1].savings_diff)}`,
+            increase: statistics[statistics.length - 1].savings_diff > 0,
             icon: <MdOutlineSavings size={36} />
         },
     ];
