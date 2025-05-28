be33f0861059dddc69e648d5209bdf9db090922f
snakee0101 <snakee0101@gmail.com>
2025-05-28 18:31:58 +0300

    using notification context in the inbox page

--- resources/js/pages/inbox.tsx
+++ resources/js/pages/inbox.tsx
@@ -2,21 +2,14 @@ import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head, useForm } from '@inertiajs/react';
 
-import {
-    Toast,
-    ToastToggle,
-    createTheme,
-    Card,
-    Button
-} from 'flowbite-react';
+import { Card, Button } from 'flowbite-react';
 import { useState } from 'react';
-import { HiCheck } from 'react-icons/hi';
 
 import '../../css/app.css';
 import dayjs from 'dayjs';
 
 import { RiDeleteBin6Line } from "react-icons/ri";
-
+import { useNotification } from '@/contexts/NotificationContext';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -25,23 +18,8 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
-const toastThemeWithAbsolutePositioning = createTheme({
-    toast: {
-        root: {
-            base: 'absolute top-2 right-2 flex w-full max-w-xs items-center rounded-lg bg-white p-4 text-gray-500 shadow dark:bg-gray-800 dark:text-gray-400',
-            closed: 'opacity-0 ease-out',
-        },
-        toggle: {
-            base: '-m-1.5 ml-auto inline-flex h-8 w-8 rounded-lg bg-white p-1.5 text-gray-400 hover:bg-gray-100 hover:text-gray-900 focus:ring-2 focus:ring-gray-300 dark:bg-gray-800 dark:text-gray-500 dark:hover:bg-gray-700 dark:hover:text-white',
-            icon: 'h-5 w-5 shrink-0',
-        },
-    },
-});
-
 export default function Inbox({ notifications }) {
-    const [isNotificationShown, setIsNotificationShown] = useState(false);
-    const [notificationMessage, setNotificationMessage] = useState('');
-
+    const { showNotification } = useNotification();
     const {post} = useForm();
 
     const [selectedNotificationId, setSelectedNotificationId] = useState(notifications[0]?.id ?? null);
@@ -58,21 +36,11 @@ export default function Inbox({ notifications }) {
 
     function deleteNotification(notification_id) {
         post(route('inbox.delete_notification', {notificationId: notification_id}));
+        showNotification('Message deleted');
     }
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            {isNotificationShown && (
-                <Toast theme={toastThemeWithAbsolutePositioning.toast}>
-                    <div
-                        className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-none bg-green-100 text-green-500 dark:bg-green-800 dark:text-green-200">
-                        <HiCheck className="h-5 w-5" />
-                    </div>
-                    <div className="ml-3 text-sm font-normal">{notificationMessage}</div>
-                    <ToastToggle />
-                </Toast>
-            )}
-
             <Head title="Inbox" />
 
             <div className="flex min-h-screen">
